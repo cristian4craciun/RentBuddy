6894c021ea8ad6f7d0c6ea378d8399925789fad3
Yash Jagirdar <yjagirda@uwaterloo.ca>
2025-02-23 15:48:54 -0500

    authentication + removing old files

---
+++ client/src/components/Authorization/SignUp/callApiAddUser.js
@@ -0,0 +1,21 @@
+async function callApiAddUser(userData, idToken) {
+    try {
+      const response = await fetch('/api/addUser', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: idToken,
+        },
+        body: JSON.stringify(userData),
+      });
+      const result = await response.json();
+      if (response.status !== 200) throw Error(result.message);
+      const request = JSON.parse(result);
+      return request;
+    } catch (error) {
+      console.error('Error adding review:', error);
+    }
+  }
+  
+  export default callApiAddUser;
+  
